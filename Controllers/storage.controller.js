0f0ef192ee36295cf4e4d78fcc2112c36e247e3d
Aman <aamanpatidar110@gmail.com>
2023-04-27 11:15:34 +0530

    minor chnage

--- Controllers/storage.controller.js
+++ Controllers/storage.controller.js
@@ -5,7 +5,7 @@ const { v4: uuidv4 } = require('uuid');
 const { async } = require('@firebase/util');
 const fs = require('fs');
 const { fetchFiles, removeFile } = require('../Services/storage.service');
-const User = require("../model/user");
+const User = require('../model/user');
 const sendEmail = require('../Services/email.service');
 
 exports.downloadFile = async (req, res) => {
@@ -32,23 +32,23 @@ exports.uploadFile = async (req, res) => {
     fileName: req?.file?.originalname,
     userId: req?.user?._id,
     storageFileName: req?.file?.filename,
-    fileSize: (req?.file?.size/1048576)
+    fileSize: req?.file?.size / 1048576,
   });
 
-
-  
   const savedFile = await file.save();
   req.fileId = savedFile._id;
   User.updateOne(
-    { _id:req?.user?._id },
-    { $inc: { usedSpace: (req?.file?.size/1048576) } }
+    { _id: req?.user?._id },
+    { $inc: { usedSpace: req?.file?.size / 1048576 } }
   )
-  .then((result) => {
-    console.log(`Successfully updated usedSpace by adding ${(req?.file?.size/1048576)}`);
-  })
-  .catch((error) => {
-    console.error("Error updating user", error);
-  });
+    .then((result) => {
+      console.log(
+        `Successfully updated usedSpace by adding ${req?.file?.size / 1048576}`
+      );
+    })
+    .catch((error) => {
+      console.error('Error updating user', error);
+    });
 
   const bodyy = `
 Dear user,
@@ -60,7 +60,12 @@ If you have any questions or concerns about this file, please don't hesitate to 
 Best regards,
 IIITK Cloud Team
 `;
-  await sendEmail(to='119cs0005@iiitk.ac.in',subject ='New file uploaded to your cloud storage!',htmlbody ='New File added',body =bodyy);
+  await sendEmail(
+    (to = '119cs0005@iiitk.ac.in'),
+    (subject = 'New file uploaded to your cloud storage!'),
+    (htmlbody = 'New File added'),
+    (body = bodyy)
+  );
   res.json({
     message: 'File uploaded successfully',
     file: { ...savedFile.toObject() },
@@ -75,16 +80,12 @@ exports.getUserFile = async (req, res, next) => {
     const fileId = req.query.fileId;
 
     console.log('fetching files', limit, page, searchText, fileId);
+    const userId = req?.user?._id;
+    const response = await fetchFiles(limit, page, searchText, fileId, userId);
 
-    const response = await fetchFiles(
-      limit,
-      page,
-      searchText,
-      fileId,
-      req?.user?._id
-    );
+    const user = await User.findOne({ _id: userId });
     // console.log('channel added', response);
-    res.status(200).json({ ...response });
+    res.status(200).json({ ...response, user });
   } catch (error) {
     if (!error.statusCode) error.statusCode = 500;
     console.log(error);
